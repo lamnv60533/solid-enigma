70632becd1aa7b47611c5c5658480857d2a15aea
Liam Nguyen <lamnv60533@gmail.com>
2021-04-23 00:38:24 +0700

    fix

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("Hello, world 1122.")
+}
+
+func ReverseRunes(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
